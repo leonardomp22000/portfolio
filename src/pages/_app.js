dbb3a865b9e0a8b1f047eacd81e56795198513a0
Leonardo <leonardomp2200@gmail.com>
2025-02-03 15:09:12 -0600

    Project configuration and instalation of heroicons

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
+import { Poppins } from "next/font/google";
 import "@/styles/globals.css";
 
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"], // Agrega los pesos que necesitas
+  variable: "--font-poppins", // Define una variable CSS
+});
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={poppins.variable}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
