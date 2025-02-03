2ee0ec16486b273902de6391532f51314af18f92
Leonardo <leonardomp2200@gmail.com>
2025-02-03 15:12:29 -0600

    Integration of OffCanvas and downloadButton done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import Button from "@/components/Button";
+import Menu from "@/components/Menu";
+function download() {
+  const link = document.createElement("a"); // Crea un elemento a
+  link.href = "/CVLeonardoMeza.pdf"; // Usa la ruta correcta Epecifica la ruta donde esta el archivo de descarga
+  link.download = "CVLeonardoMeza.pdf"; //Indica que se trata de una descarga
+  document.body.appendChild(link); // Inserta el enlace en el body del documento
+  link.click(); // Simula un click por el usuario para iniciar la descarga
+  document.body.removeChild(link); // Elimina el enlace del DOM limpiar la memoria y evitar elementos innecesarios
+}
+export default function Navbar() {
+  return (
+    <nav className="border p-3 border-red-500 max-w-sm flex justify-end gap-4 mx-auto ">
+      <Button handleFunction={download}>Download CV</Button>
+      <Menu />
+    </nav>
+  );
+}
