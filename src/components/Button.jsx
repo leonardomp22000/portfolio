4cccecc4935653a10fc8588af82be8ade9d56e2d
Leonardo <leonardomp2200@gmail.com>
2025-02-04 17:40:39 -0600

    Creation of Project Section with projects cards and buttons variants

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,25 @@
-export default function Button({ children, handleFunction }) {
+import clsx from "clsx";
+
+const buttonStyles = {
+  primary: "bg-[#fd6f00] hover:bg-[#ff890a] active:bg-[#cc5002] text-white",
+  secondary:
+    "bg-[#ffffff] hover:bg-[#FD6F00] hover:text-white  active:bg-[#cc5002] text-black border !rounded-xl border-gray-400",
+  outline:
+    "border border-[#fd6f00] text-[#fd6f00] hover:bg-[#fd6f00] hover:text-white",
+};
+
+export default function Button({
+  children,
+  handleFunction,
+  variant = "primary",
+}) {
   return (
     <button
       onClick={handleFunction}
-      className=" rounded-md bg-[#fd6f00] hover:bg-[#ff890a] active:bg-[#cc5002] px-4 py-2 text-white font-poppins font-normal text-base tracking-wide"
+      className={clsx(
+        "rounded-md px-4 py-2 font-poppins font-normal text-base tracking-wide transition-colors duration-200",
+        buttonStyles[variant]
+      )}
     >
       {children}
     </button>
---
+++ src/components/Projects.jsx
@@ -0,0 +1,83 @@
+import Title from "./Title";
+import Paragraph from "./Paragraph";
+import Button from "./Button";
+import ProjectCard from "./ProjectCard";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import { Pagination } from "swiper/modules";
+
+const buttons = [
+  { text: "All" },
+  { text: "UI/UX" },
+  { text: "WebDesign" },
+  { text: "App Design" },
+  { text: "Graphic Design" },
+];
+
+const projects = [
+  {
+    type: "Web Design",
+    title: "AirCalling Landing Page Design",
+    src: "/images/project1.webp",
+  },
+  {
+    type: "Web Design",
+    title: "Business Landing Page Design",
+    src: "/images/project2.webp",
+  },
+  {
+    type: "Web Design",
+    title: "Ecom Web Page Design",
+    src: "/images/project3.webp",
+  },
+];
+export default function Projects() {
+  return (
+    <>
+      <section className=" max-w-sm mx-auto p-4  text-center">
+        <Title>My Projects</Title>;
+        <Paragraph>
+          Lorem ipsum dolor sit amet consectetur. Mollis erat duis aliquam
+          mauris est risus lectus. Phasellus consequat urna tellus
+        </Paragraph>
+        <div className="p-4  overflow-x-auto whitespace-nowrap space-x-3">
+          {buttons.map((button, index) => {
+            return (
+              <Button
+                key={index}
+                variant="secondary"
+              >
+                {button.text}
+              </Button>
+            );
+          })}
+        </div>
+        <Swiper
+          modules={[Pagination]}
+          spaceBetween={20}
+          slidesPerView={1}
+          loop={true}
+          pagination={{ clickable: true }}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log("slide change")}
+        >
+          {projects.map((project, index) => {
+            return (
+              <SwiperSlide key={index}>
+                <ProjectCard
+                  type={project.type}
+                  title={project.title}
+                  image={project.src}
+                />
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </section>
+    </>
+  );
+}
